f79da0ca082f792cdb1593636e458c63b72b7b09
rahathasanDev <devrahat.hasan@gmail.com>
2024-05-15 12:19:02 +0600

    Modified homepage

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ const HomePage = () => {
   console.log("rahat hasan");
   return (
    <div className="m-10 flex flex-col ">
-    <h1 className="mx-auto font-semibold">This is NEXT JS homepage</h1>
+    <h1 className="mx-auto font-semibold">This is NEXT JS homepage !!!</h1>
     <Counter></Counter>
    </div>
 

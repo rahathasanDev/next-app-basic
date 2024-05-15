e9ae3bb0fae4119ee9e43e1e0eb8a6e47100fcbe
rahathasanDev <devrahat.hasan@gmail.com>
2024-05-15 12:16:12 +0600

    basic next app and features

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+import Counter from "@/components/Counter/Counter";
+
+const HomePage = () => {
+  console.log("rahat hasan");
+  return (
+   <div className="m-10 flex flex-col ">
+    <h1 className="mx-auto font-semibold">This is NEXT JS homepage</h1>
+    <Counter></Counter>
+   </div>
+
+  );
+};
+
+export default HomePage;
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+
+      <div className="flex flex-col mx-auto gap-3 mt-5 w-1/3 ">
+        
+        <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>Increase</button>
+        <button className="btn btn-outline">Counter {counter}</button >
+        <button className="btn btn-neutral" onClick={() => setCounter(counter - 1)}>Decrease</button>
+      </div>
+   
+  );
+};
+
+export default Counter;

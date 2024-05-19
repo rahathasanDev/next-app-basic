0d9baedd2c2c5e6fa4315b247ef1f716060ee4ae
rahathasanDev <devrahat.hasan@gmail.com>
2024-05-19 20:27:09 +0600

    Data fetching and Card data updated

--- src/app/page.js
+++ src/app/page.js
@@ -11,19 +11,19 @@ const HomePage = async () => {
     <div className="m-10 flex flex-col">
       <h1 className="mx-auto font-semibold">This is NEXT JS homepage !!!</h1>
       <Counter></Counter>
-      <div className="flex justify-between p-5">
+      <div className="md:flex justify-around  p-5 ">
         {CyberData.slice(0,3).map((data) => (
-          <div key={data.id} className="card w-80 bg-base-100 shadow-xl">
+          <div key={data.id} className="card w-96 mb-10 mt-10 bg-base-100 shadow-xl">
             <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
             <div className="card-body">
               <h2 className="card-title">
-                Shoes!
-                <div className="badge badge-secondary">NEW</div>
+               {data.title}
+                <div className="badge badge-secondary">{data.price}</div>
               </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>{data.description}</p>
               <div className="card-actions justify-end">
-                <div className="badge badge-outline">Fashion</div>
-                <div className="badge badge-outline">Products</div>
+                <div className="btn btn-outline">See more</div>
+                <div className="btn btn-outline">Buy Now</div>
               </div>
             </div>
           </div>
